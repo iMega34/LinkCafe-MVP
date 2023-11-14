180cb20bc80f40707b38736304fc493ce43fe2bf
iMega34 <mega2207@outlook.com>
2023-11-13 22:23:10 -0600

    Título y contador de órdenes añadidos

--- styles/s_orders.py
+++ styles/s_orders.py
@@ -2,11 +2,27 @@
 import flet as ft
 
 from styles.styles import Styles
+from other.db_connection import DBConnection
 
 
 # Propiedades de estilo de la página de órdenes, se obtienen de la clase
 # Styles del archivo styles.py
 styles: dict[str] = Styles.orders_styles()
+
+
+# Conexión con la base de datos
+db_connection: DBConnection = DBConnection()
+# Lista de órdenes
+orders: dict[str] = db_connection.get_orders()
+# Número de órdenes en el local
+pos_orders: int = 0
+# Número de órdenes de Rappi
+rappi_orders: int = 0
+# Número de órdenes de menú digital
+digital_menu_orders: int = 0
+
+# Objeto de la clase ft.ListView para contener los productos del catálogo
+_list_view: ft.ListView = ft.ListView()
 
 
 class SOrders:
@@ -15,4 +31,215 @@ class SOrders:
     del archivo :file:`orders.py` para la creación de la página de órdenes.
     """
 
-    
+    def _build_order_list(self) -> None:
+        """
+        Construye la lista de órdenes.
+
+        Parámetros:
+            - :param:`page` (ft.Page): Página actual.
+
+        Regresa:
+            - No regresa ningún valor.
+        """
+
+        _order_id: ft.Container = ft.Container(
+            alignment = ft.alignment.center,
+            content = ft.Text(
+
+            )
+        )
+
+        for id, details in orders.items():
+            card_content: ft.Container = ft.Container(
+                border_radius = ft.border_radius.all(styles["card"]["border_radius"]),
+                width = styles["card"]["width"],
+                bgcolor = styles["card"]["bgcolor"],
+                padding = styles["card"]["padding"],
+                shadow = ft.BoxShadow(
+                    color = "#000000",
+                    offset = ft.Offset(0, 12)
+                ),
+                # Contenido de la tarjeta de la orden
+                content = ft.Column(
+                    alignment = ft.MainAxisAlignment.CENTER,
+                    controls = [
+
+                    ]
+                )
+            )
+
+
+    def _calculate_order_quantity_by_origin(self) -> None:
+        """
+        Calcula la cantidad de órdenes por origen.
+
+        Parámetros:
+            - No recibe parámetros.
+
+        Regresa:
+            - No regresa ningún valor.
+        """
+
+        global pos_orders, rappi_orders, digital_menu_orders
+
+        pos_orders = rappi_orders = digital_menu_orders = 0
+
+        for order in orders.values():
+            if order["origin"] == "Local":
+                pos_orders += 1
+            elif order["origin"] == "Rappi":
+                rappi_orders += 1
+            elif order["origin"] == "Menú digital":
+                digital_menu_orders += 1
+
+
+    def _subcounter(self, origin: str, quantity: int, tag_color: str) -> ft.Container:
+        """
+        Crea los contadores de órdenes por origen.
+
+        Parámetros:
+            - :param:`origin` (str): Origen de la orden.
+            - :param:`quantity` (int): Cantidad de órdenes por origen.
+            - :param:`tag_color` (str): Color de la etiqueta.
+
+        Regresa:
+            - :return:`counter` (ft.Container): Contador de órdenes por origen.
+        """
+
+        return ft.Container(
+            alignment = ft.alignment.center,
+            content = ft.Row(
+                spacing = styles["counter"]["spacing"],
+                alignment = ft.MainAxisAlignment.CENTER,
+                controls = [
+                    ft.Container(
+                        width = styles["counter"]["tag_width"],
+                        height = styles["counter"]["tag_height"],
+                        bgcolor = tag_color,
+                        border_radius = ft.border_radius.only(
+                            bottom_left = styles["counter"]["tag_border_radius"],
+                            bottom_right = styles["counter"]["tag_border_radius"]
+                        ),
+                    ),
+                    ft.Text(
+                        f"{origin}: {quantity}",
+                        font_family = styles["counter"]["font"],
+                        size = styles["counter"]["font_size"],
+                        color = styles["counter"]["font_color"],
+                        weight = ft.FontWeight.W_300,
+                        text_align = ft.TextAlign.CENTER
+                    )
+                ]
+            )
+        )
+
+
+    def title() -> ft.Container:
+        """
+        Título de la lista de órdenes.
+
+        Parámetros:
+            - No recibe parámetros.
+
+        Regresa:
+            - :return:`title_content` (ft.Container): Título de la lista de órdenes
+        """
+
+        title_content: ft.Container = ft.Container(
+            alignment = ft.alignment.center,
+            content = ft.Text(
+                "Órdenes",
+                font_family = styles["title"]["font"],
+                size = styles["title"]["font_size"],
+                color = styles["title"]["font_color"],
+                weight = ft.FontWeight.W_300,
+                text_align = ft.TextAlign.CENTER
+            )
+        )
+
+        return title_content
+
+
+    def active_counter(self) -> ft.Container:
+        """
+        Contador de órdenes activas totales y por origen.
+
+        Parámetros:
+            - :param:`page` (ft.Page): Página actual.
+
+        Regresa:
+            - :return:`counter_content` (ft.Container): Contador de órdenes activas
+        """
+
+        # Calcula la cantidad de órdenes por origen
+        self._calculate_order_quantity_by_origin()
+
+        # Contador de órdenes activas totales
+        total_counter: ft.Container = ft.Container(
+            alignment = ft.alignment.center,
+            content = ft.Text(
+                f"Total activos: {len(orders)}",
+                font_family = styles["counter"]["font"],
+                size = styles["counter"]["font_size"],
+                color = styles["counter"]["font_color"],
+                weight = ft.FontWeight.W_300,
+                text_align = ft.TextAlign.CENTER
+            )
+        )
+
+        # Contador de órdenes activas por caja
+        point_of_sale_counter: ft.Container = self._subcounter("Local", pos_orders, styles["counter"]["pos_color"])
+
+        # Contador de órdenes activas por Rappi
+        rappi_counter: ft.Container = self._subcounter("Rappi", rappi_orders, styles["counter"]["rappi_color"])
+
+        # Contador de órdenes activas por menú digital
+        digital_menu_counter: ft.Container = self._subcounter("Menú digital", digital_menu_orders, styles["counter"]["digital_menu_color"])
+
+        # Contenedor de los contadores
+        counter_content: ft.Container = ft.Container(
+            width = styles["counter"]["width"],
+            alignment = ft.alignment.center,
+            content = ft.Column(
+                alignment = ft.MainAxisAlignment.CENTER,
+                controls = [
+                    # Contador de órdenes activas totales
+                    total_counter,
+                    # Contador de órdenes activas por caja, Rappi y menú digital
+                    ft.Row(
+                        alignment = ft.MainAxisAlignment.SPACE_AROUND,
+                        controls = [
+                            point_of_sale_counter,
+                            rappi_counter,
+                            digital_menu_counter
+                        ]
+                    )
+                ]
+            )
+        )
+
+        return counter_content
+
+
+    def order_list(self) -> ft.Container:
+        """
+        Lista de órdenes que se han enviado al SCD y se mostrarán en la página de órdenes
+        como tarjetas.
+
+        Parámetros:
+            - :param:`page` (ft.Page): Página actual.
+
+        Regresa:
+            - :return:`order_list_content` (ft.Container): Lista de tarjetas con las órdenes
+        """
+
+        self._build_order_list()
+
+        order_list_content: ft.Container = ft.Container(
+            border = ft.border.all(1, "#FF0000"),
+            expand = True,
+            content = _list_view
+        )
+
+        return order_list_content
+
--- views/orders.py
+++ views/orders.py
@@ -1,8 +1,7 @@
 
 import flet as ft
 
-from styles.s_cashier import SCashier
-from styles.styles import Styles
+from styles.s_orders import SOrders
 
 
 def Orders(page: ft.Page) -> ft.Column:
@@ -16,3 +15,41 @@ def Orders(page: ft.Page) -> ft.Column:
 
     Regresa un objeto de la clase :class:`ft.Column`
     """
+
+    # Título de la lista de órdenes
+    title: ft.Container = SOrders.title()
+    # Contador de órdenes
+    orders_counter: ft.Container = SOrders().active_counter()
+    # Lista de órdenes
+    order_list: ft.Container = SOrders().order_list()
+
+    # Propiedades de la página de órdenes
+    view: ft.Column = ft.Column(
+        spacing = 25,
+        # Se compone de:
+        # - Título de la lista de órdenes
+        # - Lista de órdenes
+        controls = [
+            ft.Container(
+                padding = 25,
+                expand = True,
+                content = ft.Column(
+                    alignment = ft.MainAxisAlignment.CENTER,
+                    controls = [
+                        # Título de la lista de órdenes y contador de órdenes
+                        ft.Row(
+                            alignment = ft.MainAxisAlignment.SPACE_BETWEEN,
+                            controls = [
+                                title,
+                                orders_counter
+                            ]
+                        ),
+                        # Lista de órdenes
+                        order_list
+                    ]
+                )
+            )
+        ]
+    )
+
+    return view
