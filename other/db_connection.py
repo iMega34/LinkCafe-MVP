c8be40022b6c1bbcab73fb84c24cb297fda6892c
iMega34 <mega2207@outlook.com>
2023-11-13 22:22:14 -0600

    Obtiene órdenes de la BD y parsea los datos

--- other/db_connection.py
+++ other/db_connection.py
@@ -39,7 +39,7 @@ class DBConnection:
             - :return:`db_info` (tuple[str]): Tupla con la información de la base de datos
         """
 
-        with open("other\db_info.txt", "r") as file:
+        with open("other/db_info.txt", "r") as file:
             db_info: tuple[str] = tuple(file.read().splitlines())
 
         return db_info
@@ -66,7 +66,7 @@ class DBConnection:
                 self._employees.append(name)
 
         return self._employees
-    
+
 
     def send_order_to_db(self, order: dict[str]) -> None:
         """
@@ -97,3 +97,43 @@ class DBConnection:
         # Envía la orden a la base de datos
         self._cursor.execute(sql, values)
         self.__database.commit()
+
+
+    def get_orders(self) -> dict[str, list]:
+        """
+        Obtiene las órdenes de la base de datos
+
+        Parámetros:
+            - No recibe parámetros.
+
+        Regresa:
+            - :return:`orders` (dict[str, list]): Diccionario con las órdenes, con
+            el formato ``{id : {name, products_n_quantities, total, origin}}``
+        """
+
+        # Obtiene las órdenes de la base de datos
+        self._cursor.execute("SELECT * FROM orders")
+        db_rows: list[tuple[str]] = self._cursor.fetchall()
+
+        orders: dict[str, list] = {}
+
+        # Se crea un diccionario con las órdenes
+        for order in db_rows:
+            id, name, products_n_quantities, total, origin = order[0], order[1], order[2], order[3], order[5]
+            products_n_quantities_list: list[str] = products_n_quantities.split(", ")
+            products_n_quantities_dict: dict[str, str] = {}
+
+            # Se crea un diccionario con los productos y cantidades de la orden
+            for product in products_n_quantities_list:
+                product_name, product_quantity = product.split(" x ")
+                products_n_quantities_dict[product_name] = product_quantity
+
+            # Se agrega la orden al diccionario de órdenes
+            orders[id] = {
+                "name" : name,
+                "products_n_quantities" : products_n_quantities_dict,
+                "total" : total,
+                "origin" : origin
+            }
+
+        return orders
